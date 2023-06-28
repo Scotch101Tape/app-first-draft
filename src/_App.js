4e1f86a933adf0224413babe14de39a3a71481c1
Scotch101Tape <scotch101tape@gmail.com>
2023-06-28 12:44:27 -0500

    Have react native maps working

--- src/_App.js
+++ src/_App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from "expo-status-bar"
 import { StyleSheet, Text, View } from "react-native"
+import MapView from 'react-native-maps';
 
 export default function _App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.jtart working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map}/>
     </View>
   )
 }
@@ -13,8 +13,9 @@ export default function _App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
+  map: {
+    width: "100%",
+    height: "100%",
+  }
 })
