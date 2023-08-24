2ebf1bd9f5d164e369ff80c5767c7cd07bcee4b6
Scotch101Tape <scotch101tape@gmail.com>
2023-08-23 20:27:57 -0500

    Update to demobox

--- src/components/DemoBox.js
+++ src/components/DemoBox.js
@@ -2,7 +2,9 @@ import { View, StyleSheet, Text } from "react-native"
 
 const styles = StyleSheet.create({
   container: {
-    margin: 30,
+    flex: 1,
+    margin: 10,
+    /*margin: 30,
     flex: 1,
     alignItems: "center",
     borderColor: "black",
@@ -12,12 +14,22 @@ const styles = StyleSheet.create({
     flexGrow: 1,
     //flexShrink: 1,
     //position: "relative",
-    padding: 10,
+    padding: 10,*/
+  },
+  borderedContainer: {
+    flex: 1,
+    borderColor: "black",
+    borderStyle: "dotted",
+    borderWidth: 3,
+    flexGrow: 1,
+    padding: 3,
+    //alignItems: "center"
   },
   nameText: {
-    position: "absolute",
+    /*position: "absolute",
     top: -20,
     left: -3,
+    color: "red"*/
     color: "red"
   }
 })
@@ -25,6 +37,8 @@ const styles = StyleSheet.create({
 export default function DemoBox({name, children}) {
   return <View style={styles.container}>
     <Text style={styles.nameText}>{name}</Text>
-    {children}
+    <View style={styles.borderedContainer}>
+      {children}
+    </View>
   </View>
 }
