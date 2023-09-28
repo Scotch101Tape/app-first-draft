a0edf80838f3714fc0e950506283b2494d256fd4
Scotch101Tape <scotch101tape@gmail.com>
2023-09-28 10:00:14 -0500

    Create layout.js

---
+++ src/components/layout.js
@@ -0,0 +1,17 @@
+import { View } from 'react-native'
+
+export function C({grow, style, children}) {
+  return <View style={{...style, flex: 1, flexDirection: "column", flexGrow: grow || 1}}>{children}</View>
+}
+
+export function R({grow, style, children}) {
+  return <View style={{...style, flex: 1, flexDirection: "row", flexGrow: grow || 1}}>{children}</View>
+}
+
+export function Block({color}) {
+  return <View style={{backgroundColor: color}}></View>
+}
+
+export function Container({children}) {
+  return <View style={{flex: 1}}>{children}</View>
+}
