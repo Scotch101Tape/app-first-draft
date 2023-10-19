a54ae71a0e59a5d553e5309a0017f656ff00e41b
Scotch101Tape <scotch101tape@gmail.com>
2023-10-19 18:21:31 -0500

    small thing

--- src/components/MyStatusBar.js
+++ src/components/MyStatusBar.js
@@ -1,11 +1,11 @@
 import { memo } from 'react'
-import {StatusBar, View, StyleSheet, SafeAreaView, Platform} from "react-native"
+import {StatusBar, View, StyleSheet, SafeAreaView, Platform, Appearance} from "react-native"
 import { Nothing, Rect } from './layout'
 
-function MyStatusBar({backgroundColor}) {
-  return <View style={{backgroundColor}}>
+function MyStatusBar() {
+  return <View style={{backgroundColor: "black"}}>
     <SafeAreaView>
-      <StatusBar translucent backgroundColor={backgroundColor} />
+      <StatusBar translucent backgroundColor={"black"} />
     </SafeAreaView>
     {Platform.OS === "android" ? <Rect height={StatusBar.currentHeight}/> : <Nothing/>}
   </View>
